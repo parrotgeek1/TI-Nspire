58a4e1e67b08b9393377f4e98c3d09b0f33e0eeb
parrotgeek1 <parrotgeek1@gmail.com>
2023-06-17 20:17:50 -0700

    Fix missing free() in error cases in HWRevFix

--- touchpad_proto_fix/src/main.c
+++ touchpad_proto_fix/src/main.c
@@ -5,7 +5,6 @@ void showmsg(char *mesg){
     show_msgbox("HWRevFix by parrotgeek1", mesg);
 }
 
-
 static const unsigned read_nand_31_addrs[]            = {    0x10071F5C, 0x10071EC4, 0x10071658, 0X100715E8, 0x1006E0AC, 0x1006E03C};
 static const unsigned write_nand_31_addrs[]            = {    0x10072298, 0x10072200, 0x10071994, 0x10071924, 0x1006E3E8, 0x1006E378};
 static const unsigned nand_erase_range_31_addrs[]    = {    0x100724FC, 0x10072464, 0x10071BF8, 0x10071B88, 0x1006E64C, 0x1006E5DC};
@@ -55,11 +54,13 @@ int main(void) {
 
     if(buf[0x804] != 0x0c) {
         showmsg("This program is only supported on CAS monochrome calculators. If you are having touchpad problems on another calculator, please ask me for help.");
+        free(buf);
         return 0;
     }
 
     if(buf[0x806] == 0x02) {
         showmsg("Your calculator's hardware revision is already correct. If you are having touchpad problems in spite of this, please ask me for help.");
+        free(buf);
         return 0;
     }
 
@@ -74,16 +75,17 @@ int main(void) {
 
     if(buf[0x804] != 0x0c) {
         showmsg("FATAL ERROR 1\n\nDO NOT TURN OFF YOUR CALCULATOR. ASK ME FOR HELP!");
+        free(buf);
         return 0;
     }
 
     if(buf[0x806] != 0x02) {
         showmsg("FATAL ERROR 2\n\nDO NOT TURN OFF YOUR CALCULATOR. ASK ME FOR HELP!");
+        free(buf);
         return 0;
     }
 
     free(buf);
-
 
     showmsg("SUCCESS!\n\nPress Enter to reboot your calculator.");
 
